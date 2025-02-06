e2a2f4ac61ce6f2294a8b62c3f4601d393b9d69c
Yury Notiron <not.iron.yuriy@inbox.ru>
2025-02-06 22:00:54 +0300

    Получение данных с сервера Firebase

--- src/views/PageTaskList.vue
+++ src/views/PageTaskList.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { getFirestore, query, collection, orderBy, getDocs, deleteDoc, doc } from 'firebase/firestore';
+import { useUserStore } from './../stores/user';
+import type { IInterview } from './../Interfaces';
+
+const DB = getFirestore();
+const userId = useUserStore().userId;
+
+const interviews = ref<IInterview[]>([]);
+const isLoading = ref<boolean>(true);
+
+const getAllInterviews = async <T extends IInterview>(): Promise<T[]> => {
+  const endPoint = query(collection(DB, `users/${userId}/interviews`), orderBy('dateCreation', 'asc'));
+  const getData = await getDocs(endPoint);
+  return getData.docs.map(doc => doc.data() as T);
+};
+
+onMounted(async () => {
+  const listInervievs: IInterview[] = await getAllInterviews();
+  interviews.value = listInervievs;
+});
+</script>
 
 <template></template>
-
 <style></style>
